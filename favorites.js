0305dbfa3a5152b0805e97b15b1bcfa279d17dd2
Jonathan Green <artistjongreen@gmail.com>
2018-12-12 18:01:22 -0600

    Created JavaScript

--- favorites.js
+++ favorites.js
@@ -1,1 +1,31 @@
-gjhgj
+// prints message in Chrome developer tools to prove it worked
+console.log("favorites.js loaded");
+
+function DrawFavorites() {
+
+  var things =[
+    "iguanas",
+    "edm",
+    "food",
+    "pokemon",
+    "nature"
+  ];
+
+  var targetElement = document.getElementById("favorites");
+
+  for (var i=0;i<things.length;i++){
+
+    var thing = things[i];
+
+    var listItem = document.createElement("li");
+
+    listItem.innerText = thing;
+
+    targetElement.appendChild(listItem);
+
+  }
+
+}
+
+// this runs the "DrawFavorites" function when the web page loads
+window.onload = DrawFavorites;
